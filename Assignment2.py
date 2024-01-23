09b2dce31d0752b3faad577bb37ba9ec0f46f246
KevinBWinter <33888086+KevinBWinter@users.noreply.github.com>
2024-01-23 16:54:45 -0500

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,78 @@
+import re
+import socket
+ 
+class Assignment2:
+    def __init__(self, year):
+        self.year = year
+    def tellAge(self, currentYear):
+        age = currentYear - self.year
+        print(f"Your age is {age}")
+    def listAnniversaries(self):
+        currentYear = 2024
+        yearsPassed = currentYear - self.year
+        anniversaries = []
+        for i in range(10, yearsPassed + 1, 10):
+            anniversaries.append(i)
+        return anniversaries
+    def modifyYear(self, n):
+        year_str = str(self.year)
+
+        first_part = year_str[:2] * n
+ 
+     
+        second_part = ''.join([year_str[i] for i in range(0, len(year_str), 2)]) * n
+ 
+        return first_part + second_part
+    @staticmethod
+    def checkGoodString(string):
+        if len(string) < 9:
+            return False
+ 
+      
+        if not string[0].islower():
+            return False
+ 
+     
+        if len(re.findall(r'\d', string)) != 1:
+            return False
+ 
+        return True
+    @staticmethod
+    def connectTcp(host, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+          
+            sock.connect((host, port))
+ 
+        
+            sock.close()
+ 
+            return True
+        except:
+            return False
+
+ 
+ 
+x = Assignment2(1996)
+ 
+x.tellAge(2024)
+ 
+y = x.listAnniversaries()
+print(y)
+ 
+ret = x.modifyYear(3)
+print(ret)
+ 
+ret = Assignment2.checkGoodString("f1obar0more")
+print(ret)  # Should print False
+ 
+ret = Assignment2.checkGoodString("foobar0more")
+print(ret)  # Should print True
+ 
+retval = Assignment2.connectTcp("www.google.com", 80)
+if retval:
+    print("Connection established correctly")
+else:
+    print("Some error")
+ 
